08356f2811aa4d7466f875d7f5201f6fc954aa88
Tzu-Han Lin <102434871+tzuhan2424@users.noreply.github.com>
2024-04-21 16:59:15 -0400

    env production, frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,18 @@ import MainApp from "./module/MainApp";
 import Footer from "./module/Footer";
 import "./module/css/app.css";
 function App() {
+
+  const environment = process.env.REACT_APP_APP_ENV;
+  console.log(environment)
   return (
     <div className = 'major-container'>
+      <div>
+      {environment === 'production' ? (
+        <p>This is running in Production.</p>
+      ) : (
+        <p>This is running in Development.</p>
+      )}
+    </div>
       <Header/>
       <MainApp/>
       <Footer/>
